cb0032da58ac163731270441ea72f6cbf7362d5e
Mbarike <thiamhawa361@gmail.com>
2026-03-28 14:05:11 +0000

    VoletNavbar

--- src/composants/Navbar.jsx
+++ src/composants/Navbar.jsx
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <h2 className="logo">Hawa Thiam</h2>
 
-      <ul className="nav-links">
-        <li><Link to="/">Accueil</Link></li>
-        <li><Link to="/apropos">À propos</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+      {/* BOUTON HAMBURGER */}
+      <div className="menu-icon" onClick={() => setOpen(!open)}>
+        ☰
+      </div>
+
+      {/* NAV LINKS */}
+      <ul className={`nav-links ${open ? "active" : ""}`}>
+        <li><Link to="/" onClick={() => setOpen(false)}>Accueil</Link></li>
+        <li><Link to="/apropos" onClick={() => setOpen(false)}>À propos</Link></li>
+        <li><Link to="/projects" onClick={() => setOpen(false)}>Projects</Link></li>
+        <li><Link to="/contact" onClick={() => setOpen(false)}>Contact</Link></li>
       </ul>
     </nav>
   );
